e4bda5608d59fb3188b2559ce5ebc7160034e521
phimadavid <220028323+phimadavid@users.noreply.github.com>
2025-07-11 07:43:51 +0800

    feat: changes on configuration on build

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,33 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+    serverComponentsExternalPackages: ["sequelize", "@sequelize/core"],
+  },
+  headers: () => [
+    {
+      source: "/api/:path*",
+      headers: [
+        {
+          key: "Cache-Control",
+          value: "no-store",
+        },
+      ],
+    },
+  ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+        port: "",
+        pathname: "/photos/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
